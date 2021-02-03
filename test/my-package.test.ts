d290b66ef05bdcb70494e12a71741eca717e3ff9
silan.wu <silan.wu@liulishuo.com>
2021-02-03 20:54:26 +0800

    init

---
+++ test/my-package.test.ts
@@ -0,0 +1,14 @@
+import DummyClass from "../src/my-package"
+
+/**
+ * Dummy test
+ */
+describe("Dummy test", () => {
+  it("works if true is truthy", () => {
+    expect(true).toBeTruthy()
+  })
+
+  it("DummyClass is instantiable", () => {
+    expect(new DummyClass()).toBeInstanceOf(DummyClass)
+  })
+})
